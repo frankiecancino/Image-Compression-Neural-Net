ea7b5672b2d7053642836b099a2e6bbb5a6687c0
frankiecancino <frankiecancino@gmail.com>
2017-05-06 22:10:25 -0500

    added save and load model functionality

--- imageCompressionNeuralNet.py
+++ imageCompressionNeuralNet.py
@@ -35,56 +35,28 @@ x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
 x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
 
 # def train():
-    autoencoder.fit(x_train, x_train,
-                    epochs=50,
-                    batch_size=128,
-                    shuffle=True,
-                    validation_data=(x_test, x_test),
-                    callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+autoencoder.fit(x_train, x_train,
+                epochs=2,
+                batch_size=128,
+                shuffle=True,
+                validation_data=(x_test, x_test),
+                callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
 
 decoded_imgs = autoencoder.predict(x_test)
 
-n = 10
-plt.figure(figsize=(20, 4))
-for i in range(n):
-    # display original
-    ax = plt.subplot(2, n, i)
-    plt.imshow(x_test[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+autoencoder_json = autoencoder.to_json()
+with open("autoencoder.json", "w") as json_file:
+    json_file.write(autoencoder_json)
+print("Saved autoencoder model to disk")
 
-    # display reconstruction
-    ax = plt.subplot(2, n, i + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
+json_file = open('autoencoder.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+print("Loaded autoencoder model from disk")
+loaded_model.compile(optimizer='adadelta', loss='binary_crossentropy')
 
-n = 10
-plt.figure(figsize=(20, 8))
-for i in range(n):
-    ax = plt.subplot(1, n, i)
-    plt.imshow(encoded_imgs[i].reshape(4, 4 * 8).T)
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
 
-# autoencoder_json = autoencoder.to_json()
-# with open("autoencoder.json", "w") as json_file:
-#     json_file.write(autoencoder_json)
-# print("Saved autoencoder model to disk")
-#
-# json_file = open('autoencoder.json', 'r')
-# loaded_model_json = json_file.read()
-# json_file.close()
-# loaded_model = model_from_json(loaded_model_json)
-# print("Loaded autoencoder model from disk")
-# loaded_model.compile(optimizer='adadelta', loss='binary_crossentropy')
-#
-#
 # n = 10
 # plt.figure(figsize=(20, 4))
 # for i in range(n):
