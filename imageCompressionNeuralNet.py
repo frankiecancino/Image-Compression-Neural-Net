246ccb06ab0bcd78d90a30ade16c82cb13c21bb5
frankiecancino <frankiecancino@gmail.com>
2017-05-10 16:56:05 -0500

    sams bullshit

--- imageCompressionNeuralNet.py
+++ imageCompressionNeuralNet.py
@@ -34,47 +34,49 @@ x_test = x_test.astype('float32') / 255.
 x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
 x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
 
-# def train():
-autoencoder.fit(x_train, x_train,
-                epochs=2,
-                batch_size=128,
-                shuffle=True,
-                validation_data=(x_test, x_test),
-                callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+def train(autoencoder):
+    autoencoder.fit(x_train, x_train,
+                    epochs=3,
+                    batch_size=128,
+                    shuffle=True,
+                    validation_data=(x_test, x_test),
+                    callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
 
-decoded_imgs = autoencoder.predict(x_test)
+    autoencoder_json = autoencoder.to_json()
+    with open("autoencoder.json", "w") as json_file:
+        json_file.write(autoencoder_json)
+    autoencoder.save_weights("model.h5")
+    print("Saved autoencoder model to disk")
 
-autoencoder_json = autoencoder.to_json()
-with open("autoencoder.json", "w") as json_file:
-    json_file.write(autoencoder_json)
-print("Saved autoencoder model to disk")
 
-json_file = open('autoencoder.json', 'r')
+json_file = open('encoder.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights("model.h5")
+decoded_imgs = loaded_model.predict(x_test)
 print("Loaded autoencoder model from disk")
-loaded_model.compile(optimizer='adadelta', loss='binary_crossentropy')
+#loaded_model.compile(optimizer='adadelta', loss='binary_crossentropy')
 
 
-# n = 10
-# plt.figure(figsize=(20, 4))
-# for i in range(n):
-#     # display original
-#     ax = plt.subplot(2, n, i)
-#     plt.imshow(x_test[i].reshape(28, 28))
-#     plt.gray()
-#     ax.get_xaxis().set_visible(False)
-#     ax.get_yaxis().set_visible(False)
-#
-#     # display reconstruction
-#     ax = plt.subplot(2, n, i + n)
-#     plt.imshow(decoded_imgs[i].reshape(28, 28))
-#     plt.gray()
-#     ax.get_xaxis().set_visible(False)
-#     ax.get_yaxis().set_visible(False)
-# plt.show()
-#
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    #ax = plt.subplot(2, n, i)
+    #plt.imshow(x_test[i].reshape(28, 28))
+    #plt.gray()
+    #ax.get_xaxis().set_visible(False)
+    #ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    #ax = plt.subplot(2, n, i + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    #ax.get_xaxis().set_visible(False)
+    #ax.get_yaxis().set_visible(False)
+plt.show()
+
 # n = 10
 # plt.figure(figsize=(20, 8))
 # for i in range(n):
