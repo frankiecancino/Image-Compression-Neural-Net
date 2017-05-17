4189b454b127ce82eae6744695c294100aef69c6
frankiecancino <frankiecancino@gmail.com>
2017-05-17 11:47:11 -0500

    final version

--- imageCompressionNeuralNet.py
+++ imageCompressionNeuralNet.py
@@ -6,6 +6,29 @@ from keras.callbacks import TensorBoard
 from keras.models import model_from_json
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
+import os
+
+DATA = "./images/"
+
+def save_mnist():
+    (x_train, _), (x_test, _) = mnist.load_data()
+
+    i = 0
+    for img in x_test:
+        im = Image.fromarray(img)
+        im.save(DATA + "img" + str(i) + ".jpeg")
+        i += 1
+
+def load_mnist():
+    x_test = []
+    for file in os.listdir(DATA):
+        if file.endswith(".jpeg"):
+            img = Image.open(DATA + file)
+            x_test.append(np.array(img.copy()))
+            img.close()
+    return x_test
+
 
 input_img = Input(shape=(28, 28, 1))
 
@@ -24,10 +47,15 @@ x = Conv2D(16, (3, 3), activation='relu')(x)
 x = UpSampling2D((2, 2))(x)
 decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
+
 autoencoder = Model(input_img, decoded)
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
 (x_train, _), (x_test, _) = mnist.load_data()
+
+x_test = np.array(load_mnist())
+
+print(x_test.shape)
 
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
@@ -36,7 +64,7 @@ x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
 
 def train(autoencoder):
     autoencoder.fit(x_train, x_train,
-                    epochs=3,
+                    epochs=50,
                     batch_size=128,
                     shuffle=True,
                     validation_data=(x_test, x_test),
@@ -48,41 +76,32 @@ def train(autoencoder):
     autoencoder.save_weights("model.h5")
     print("Saved autoencoder model to disk")
 
+# train(autoencoder)
 
-json_file = open('encoder.json', 'r')
+json_file = open('autoencoder.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 loaded_model.load_weights("model.h5")
+
 decoded_imgs = loaded_model.predict(x_test)
 print("Loaded autoencoder model from disk")
-#loaded_model.compile(optimizer='adadelta', loss='binary_crossentropy')
-
 
 n = 10
 plt.figure(figsize=(20, 4))
 for i in range(n):
     # display original
-    #ax = plt.subplot(2, n, i)
+    f, (ax1, ax2) = plt.subplots(1, 2)
     #plt.imshow(x_test[i].reshape(28, 28))
     #plt.gray()
-    #ax.get_xaxis().set_visible(False)
-    #ax.get_yaxis().set_visible(False)
+    ax1.imshow(x_test[i].reshape(28, 28))
+    # ax.get_xaxis().set_visible(False)
+    # ax.get_yaxis().set_visible(False)
 
     # display reconstruction
     #ax = plt.subplot(2, n, i + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    ax2.imshow(decoded_imgs[i].reshape(28, 28))
     plt.gray()
     #ax.get_xaxis().set_visible(False)
     #ax.get_yaxis().set_visible(False)
 plt.show()
-
-# n = 10
-# plt.figure(figsize=(20, 8))
-# for i in range(n):
-#     ax = plt.subplot(1, n, i)
-#     plt.imshow(encoded_imgs[i].reshape(4, 4 * 8).T)
-#     plt.gray()
-#     ax.get_xaxis().set_visible(False)
-#     ax.get_yaxis().set_visible(False)
-# plt.show()
